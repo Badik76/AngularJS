1fcde4d7693218afd8cdc3c889ce98f41e611f33
Karl <badik76@gmail.Comgit>
2018-10-30 16:51:41 +0100

    Ajout json + exo 3

--- bonus/exo2/assets/js/script.js
+++ bonus/exo2/assets/js/script.js
@@ -1,7 +1,7 @@
-$(function() {
-
-
-
-
-
-});
+angular.module('alertApp', [])
+  .controller('alertCtrl', ['$scope', '$window', function($scope, $window) {
+    $scope.greeting = 'Tu as un ananas dans le babas!';
+    $scope.doGreeting = function(greeting) {
+      $window.alert(greeting);
+    };
+  }]);
